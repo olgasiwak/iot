58af2d31b4ac4d96701705ab3161485d23d59a73
SebastianKulig <kuligsebastian8@gmail.com>
2022-11-14 20:31:42 +0100

    + add collector mechanism

--- scripts/collector/collector_executor.py
+++ scripts/collector/collector_executor.py
@@ -1,6 +1,7 @@
+from enum import Enum
+
 import paho.mqtt.client as mqtt
 import threading
-import time
 import json
 import pprint
 import socket
@@ -13,28 +14,67 @@ ENCODING = 'utf-8'
 STATES = {}
 BACKUP_STATEFILE = 'states.backup'
 
+
+class LampState(str, Enum):
+    on = 1
+    off = 0
+
+
+class Lamp:
+    def __init__(self, name: str, state: LampState) -> None:
+        self.name = name
+        self.state = state
+
+
+def change_lamps_states(lamp):
+    if lamp.state == LampState.on:
+        send_on_signal(lamp.name)
+    elif lamp.state == LampState.off:
+        send_off_signal(lamp.name)
+    else:
+        print("Wrong message format")
+
+
+def send_on_signal(lamp_name):
+    print(f'Send on signal to {lamp_name}')
+    client = mqtt.Client()
+    client.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_TIMEOUT)
+    client.publish(lamp_name, LampState.on.value)
+
+
+def send_off_signal(lamp_name):
+    print(f'Send off signal to {lamp_name}')
+    client = mqtt.Client()
+    client.publish(lamp_name, LampState.off.value)
+
+
 def on_connect(client, userdata, flags, rc):
     print(f'Connected with result code {str(rc)}')
     client.subscribe('sensors/+')
+
 
 def on_message(client, userdata, msg):
     if msg.topic not in STATES:
         STATES[msg.topic] = msg.payload.decode(ENCODING)
     elif STATES[msg.topic] != msg.payload.decode(ENCODING):
         STATES[msg.topic] = msg.payload.decode(ENCODING)
-        notify_executor()
+        state = int(STATES[msg.topic])
+        notify_executor(Lamp(msg.topic, LampState(state)))
     print(pprint.pprint(STATES))
 
-def notify_executor():
+
+def notify_executor(lamp):
     print('executor notified')
-    #call executor class there
+    change_lamps_states(lamp)
     pass
+
 
 def notify_data_to_metric():
     print('data2metric notified')
-    #call date2metric class there
+    # call date2metric class there
     threading.Timer(5.0, notify_data_to_metric()).start()
     pass
+
 
 def main():
     client = mqtt.Client()
@@ -42,13 +82,17 @@ def main():
     client.on_message = on_message
     client.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_TIMEOUT)
     try:
-        #notify_data_to_metric()
+        # notify_data_to_metric()
         client.loop_forever()
     except KeyboardInterrupt:
         with open(BACKUP_STATEFILE, 'w') as backup:
             backup.write(json.dumps(STATES))
         print('disconnected')
 
+
 if __name__ == "__main__":
     main()
 
+# TODO convert into object oriented program
+# TODO prepare a mechanism for switching on entire sections of lamps
+# TODO prepare a mechanism for storing previous state of sensors
