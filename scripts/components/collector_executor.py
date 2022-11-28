e11a559d0c89a9ca5b7ddf81467374096c63b450
SirPatrykKawa <kawapatryk99@gmail.com>
2022-11-28 20:30:30 +0100

    added multithreading to collector

--- scripts/components/collector_executor.py
+++ scripts/components/collector_executor.py
@@ -1,4 +1,5 @@
 import json
+import time
 import pprint
 import re
 import threading
@@ -10,6 +11,7 @@ from lamp import LampState
 from data2metric import InfluxClient
 
 STATES = {}
+LOCK = threading.Lock()
 
 
 class Module:
@@ -43,6 +45,8 @@ class Module:
         self.client.subscribe(config.TOPIC_SENSORS_COLLECTOR_REGEX)
 
     def on_message(self, client, userdata, msg):
+        global LOCK
+        LOCK.acquire()
         if msg.topic not in STATES:
             config.logger.debug(f'Adding new sensor group {msg.topic}')
             STATES[msg.topic] = msg.payload.decode(config.ENCODING)
@@ -50,26 +54,33 @@ class Module:
             STATES[msg.topic] = msg.payload.decode(config.ENCODING)
             state = int(STATES[msg.topic])
             self.notify_executor(Lamp(msg.topic, LampState(state)))
-        self.notify_data_to_metric(STATES)
         config.logger.debug(pprint.pprint(STATES))
+        LOCK.release()
 
     def notify_executor(self, lamp):
         config.logger.debug('executor notified')
         self.change_lamps_states(lamp)
         pass
 
-    def notify_data_to_metric(self, states):
-        config.logger.debug('data2metric notified')
-        Influx = InfluxClient(config.INFLUX_ORG,
-                                    config.INFLUX_URL,
-                                    config.INFLUX_BUCKET,
-                                    config.INFLUX_TOKEN
-                                    )
+    def notify_data_to_metric(self, states, polling_cycle):
+        global LOCK
+        while True:
+            if not len(states):
+                continue
+            config.logger.debug('data2metric notified')
+            LOCK.acquire()
+            Influx = InfluxClient(config.INFLUX_ORG,
+                                        config.INFLUX_URL,
+                                        config.INFLUX_BUCKET,
+                                        config.INFLUX_TOKEN
+                                        )
 
-        Influx.write_to_database(
-                Influx.prepare_single_sensor_datapoints(STATES))
-        Influx.write_to_database(
-                [Influx.prepare_active_lanterns_ratio_datapoint(STATES)])
+            Influx.write_to_database(
+                    Influx.prepare_single_sensor_datapoints(STATES))
+            Influx.write_to_database(
+                    [Influx.prepare_active_lanterns_ratio_datapoint(STATES)])
+            LOCK.release()
+            time.sleep(polling_cycle)
 
     def main(self):
         self.client.on_connect = self.on_connect
@@ -85,4 +96,11 @@ class Module:
 
 if __name__ == "__main__":
     module = Module()
-    module.main()
+    thread_main = threading.Thread(target=module.main)
+    thread_data2metric = threading.Thread(target=module.notify_data_to_metric, args=(STATES, config.DATA2METRIC_POLLING_CYCLE,))
+
+    thread_main.start()
+    thread_data2metric.start()
+
+    thread_main.join()
+    thread_data2metric.join()
