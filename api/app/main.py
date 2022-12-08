3b3060e56ba69bfdf4a617e6f52fc3db668890c8
achudy <chudy.arkadiusz@gmail.com>
2022-12-08 21:53:39 +0100

    Added CORS to the app

--- api/app/main.py
+++ api/app/main.py
@@ -2,6 +2,7 @@ from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -9,6 +10,11 @@ from .database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+    )
 
 
 # Dependency
