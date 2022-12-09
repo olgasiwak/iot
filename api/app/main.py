8cbaf4e6e37f750a35ee82a3d6185ec09cf17d28
achudy <chudy.arkadiusz@gmail.com>
2022-12-09 18:53:42 +0100

    Fixed CORS methods allowed.

--- api/app/main.py
+++ api/app/main.py
@@ -14,8 +14,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*']
-    allow_methods=[""],
-    allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
     )
 
 
