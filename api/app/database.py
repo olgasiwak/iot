94b3a71209210db0bb99197ab3d2aca4ee596fd3
achudy <33877367+achudy@users.noreply.github.com>
2022-12-11 14:12:18 +0100

    Dockerized version of the API. (#14)

--- api/app/database.py
+++ api/app/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@127.0.0.1/main"
+# Baremetal
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@127.0.0.1/main"
+# Docker container
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@172.17.0.2/main"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
