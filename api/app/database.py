e044a5b83a7e3df82a900f4babcce3bb1503623b
SebastianKulig <kuligsebastian8@gmail.com>
2022-12-12 20:55:55 +0100

    # fixes

--- api/app/database.py
+++ api/app/database.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Baremetal
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@127.0.0.1/start_listening_loop"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@127.0.0.1/main"
 # Docker container
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@172.17.0.2/start_listening_loop"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@172.17.0.2/main"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
